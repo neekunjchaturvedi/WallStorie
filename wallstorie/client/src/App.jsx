fdcb7fd5b953f91f0c016fe1a28b5fc6f8b14db1
neekunjchaturvedi <neekunjchaturvedi3@gmail.com>
2025-01-13 14:53:02 +0530

    Admin view

--- wallstorie/client/src/App.jsx
+++ wallstorie/client/src/App.jsx
@@ -3,11 +3,18 @@ import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./home";
+import AuthLayout from "./components/auth/layout";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
     </Routes>
   );
 }
