c70d6ac1130b40cde00e032754cb9fc57fbd0b25
neekunjchaturvedi <neekunjchaturvedi3@gmail.com>
2025-03-08 20:39:41 +0530

    changes

--- wallstorie/server/routes/auth/auth-rotes.js
+++ wallstorie/server/routes/auth/auth-rotes.js
@@ -19,7 +19,7 @@ router.post("/register", limiter, registerUser);
 router.post("/login", limiter, loginUser);
 router.post("/logout", limiter, logoutUser);
 
-router.get("/check-auth", authMiddleware, (req, res) => {
+router.get("/check-auth", limiter, authMiddleware, (req, res) => {
   const user = req.user;
   res.status(200).json({
     success: true,
